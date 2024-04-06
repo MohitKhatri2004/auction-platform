6a014f2b2e2e862f44acf896af3277ab7014b24f
Mohit Khatri <115017719+MohitKhatri2004@users.noreply.github.com>
2024-04-06 17:10:51 +0530

    Add files via upload

---
+++ auction-platform/app/browse-auction/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+
+}
+
+const Page: React.FC<Props> = () => {
+    return (
+        <>
+            <div className="mt-60 text-white">
+            Browse Auction Page
+            </div>
+        </>
+    )
+}
+
+export default Page
---
+++ auction-platform/app/page.tsx
@@ -0,0 +1,13 @@
+import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        <Encryption />
+      </div>
+    </main>
+  );
+}
